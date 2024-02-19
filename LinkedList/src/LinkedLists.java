ab78fa21ad0be0d50d8e4ca7282767fdf256a37e
gbmk <ajetmani@gmail.com>
2024-02-18 16:42:39 -0800

    Two Sorted Linked List

--- LinkedList/src/LinkedLists.java
+++ LinkedList/src/LinkedLists.java
@@ -3,9 +3,8 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class LinkedLists {
-    List sortedList_1 = new LinkedList(Arrays.asList(2, 3, 6, 7, 8, 10));
-    List sortedList_2 = new LinkedList(Arrays.asList(-1, 3, 6, 7, 9));
-
-    List unSortedList_1 = new LinkedList(Arrays.asList(12, -3, 6, 17, 18, 10));
-    List unSortedList_2 = new LinkedList(Arrays.asList(-1, 3, 16, 17, 9));
+    public LinkedList<Integer> sortedList_1 = new LinkedList(Arrays.asList(2, 3, 6, 7, 8, 10));
+    public LinkedList<Integer> sortedList_2 = new LinkedList(Arrays.asList(-1, 3, 6, 7, 9));
+    public List unSortedList_1 = new LinkedList(Arrays.asList(12, -3, 6, 17, 18, 10));
+    public List unSortedList_2 = new LinkedList(Arrays.asList(-1, 3, 16, 17, 9));
 }
--- LinkedList/src/MergeTwoSortedLinkedList.java
+++ LinkedList/src/MergeTwoSortedLinkedList.java
@@ -1,3 +1,23 @@
-public class MergeTwoSortedLinkedList {
+import java.util.LinkedList;
 
+public class MergeTwoSortedLinkedList {
+    protected static LinkedList<Integer> mergeUnSortedList(LinkedList<Integer> list1, LinkedList<Integer> list2) {
+        LinkedList<Integer> dummyList = new LinkedList<>();
+        int index1 = 0;
+        int index2 = 0;
+        while (index1 < list1.size() && index2 < list2.size()) {
+            if (list1.get(index1) < list2.get(index2)) {
+                dummyList.add(list1.get(index1++));
+            } else {
+                dummyList.add(list2.get(index2++));
+            }
+        }
+        while (index1 < list1.size()) {
+            dummyList.add(list1.get(index1++));
+        }
+        while (index2 < list2.size()) {
+            dummyList.add(list2.get(index2++));
+        }
+        return dummyList;
+    }
 }
